b5ba2f25a77c980b59c0a488f395b4dadf5d124e
Денис Петровский <petrovsky.dv@gmail.com>
2021-02-18 20:37:10 +0300

    исправил проверку редиректа при пагинации, исправил запись json файла, исправил README.md

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -20,12 +20,14 @@ def main():
 
     args = get_arguments()
 
+    books_path = os.path.join(args.dest_folder, 'books')
+    books_images_path = os.path.join(args.dest_folder, 'images')
+    json_path = os.path.join(args.dest_folder, args.json_path)
     if not args.skip_txt:
-        books_path = os.path.join(args.dest_folder, 'books')
         os.makedirs(books_path, exist_ok=True)
     if not args.skip_imgs:
-        books_images_path = os.path.join(args.dest_folder, 'images')
         os.makedirs(books_images_path, exist_ok=True)
+    os.makedirs(args.dest_folder, exist_ok=True)
 
     books_collection_url = 'https://tululu.org/l55/'
 
@@ -48,19 +50,10 @@ def main():
             book_cards = soup.select('.ow_px_td .d_book .bookimage a')
             for book_card in book_cards:
                 book_url = urljoin(books_collection_url, book_card['href'])
-                logger.debug(f'ищем книгу по адресу {book_url}')
-                book_response = requests.get(book_url, verify=False)
-                book_response.raise_for_status()
-                parse_tululu.check_for_redirect(book_response)
-                book = parse_tululu.parse_book_page(book_response.text, book_url)
-                if not args.skip_txt:
-                    book_path = parse_tululu.download_txt(book['text_url'], book['id'], book['title'], books_path)
-                    book['book_path'] = book_path
-                if not args.skip_imgs:
-                    img_src = parse_tululu.download_image(book['image_url'], books_images_path)
-                    book['img_src'] = img_src
-                books.append(book)
-
+                book = fetch_book(args, book_url, books_images_path, books_path)
+                if book:
+                    books.append(book)
+        # ловим ошибки при попытке обработать станицу коллекции с книгами
         except requests.HTTPError as e:
             print(e, file=sys.stderr)
             logger.exception(e)
@@ -69,17 +62,43 @@ def main():
             print(e, file=sys.stderr)
             time.sleep(10)
         except requests.TooManyRedirects:
-            print('обнаружен редирект', file=sys.stderr)
+            print('Обнаружен редирект', file=sys.stderr)
         except KeyboardInterrupt:
-            print('Скачивание остановлено')
+            print('Скачивание прервано')
             sys.exit()
-        except parse_tululu.BookError as e:
-            print(e, file=sys.stderr)
 
         page_number += 1
 
-    with open(args.json_path, 'w') as file:
+    with open(json_path, 'w') as file:
         json.dump(books, file, ensure_ascii=False, indent=4)
+
+
+def fetch_book(args, book_url, books_images_path, books_path):
+    logger.info(f'ищем книгу по адресу {book_url}')
+    try:
+        book_response = requests.get(book_url, verify=False)
+        book_response.raise_for_status()
+        parse_tululu.check_for_redirect(book_response)
+        book = parse_tululu.parse_book_page(book_response.text, book_url)
+        if not args.skip_txt:
+            book_path = parse_tululu.download_txt(book['text_url'], book['id'], book['title'], books_path)
+            book['book_path'] = book_path
+        if not args.skip_imgs:
+            img_src = parse_tululu.download_image(book['image_url'], books_images_path)
+            book['img_src'] = img_src
+        return book
+    # ловим ошибки при попытке обработать карточку книги
+    except requests.HTTPError as e:
+        print(e, file=sys.stderr)
+        logger.exception(e)
+    except requests.ConnectionError as e:
+        logger.exception(e)
+        print(e, file=sys.stderr)
+        time.sleep(10)
+    except requests.TooManyRedirects:
+        print('обнаружен редирект', file=sys.stderr)
+    except parse_tululu.BookError as e:
+        print(e, file=sys.stderr)
 
 
 def get_arguments():
@@ -89,7 +108,7 @@ def get_arguments():
     parser.add_argument('--skip_imgs', action='store_true', help='не скачивать картинки')
     parser.add_argument('--skip_txt', action='store_true', help='не скачивать книги')
     parser.add_argument('--json_path', default='books.json', help='путь к json файлу с результатами')
-    parser.add_argument('--dest_folder', default='', help='путь к каталогу с результатами парсинга')
+    parser.add_argument('--dest_folder', default='static', help='путь к каталогу с результатами парсинга')
     args = parser.parse_args()
     return args
 
